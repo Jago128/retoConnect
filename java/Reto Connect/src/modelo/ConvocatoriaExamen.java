fe6ce1970880222e26dbe95d73d949773b740bd3
2dami <2dami@TartangaLH.eus>
2025-09-12 12:15:21 +0200

    adding UnidadDidactica and ConvocatoriaExamen

---
+++ java/Reto Connect/src/modelo/ConvocatoriaExamen.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author 2dami
+ */
+public class ConvocatoriaExamen {
+    private String convocatoria;
+    private String descripcion;
+    private LocalDate fecha;
+    private String curso;
+    
+    
+    public ConvocatoriaExamen(){
+        this.convocatoria="";
+        this.descripcion="";
+        this.fecha= LocalDate.now();
+        this.curso="";
+    }
+        public ConvocatoriaExamen(String convocatoria, String descripcion, LocalDate fecha, String curso){
+        this.convocatoria=convocatoria;
+        this.descripcion=descripcion;
+        this.fecha= fecha;
+        this.curso=curso;
+    }
+    public String getConvocatoria(){
+        return convocatoria;
+    }
+    public void setConvocatoria(String convocatoria){
+        this.convocatoria=convocatoria;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    @Override
+    public String toString() {
+        return "ConvocatoriaExamen{" + "convocatoria=" + convocatoria + ", descripcion=" + descripcion + ", fecha=" + fecha + ", curso=" + curso + '}';
+    }
+    
+}
