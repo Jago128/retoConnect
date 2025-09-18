8e4e58414e7cb273622f74470f87d93f9aaba512
2dami <2dami@TartangaLH.eus>
2025-09-18 13:41:57 +0200

    interface added

---
+++ java/Reto Connect/src/modelo/ImplementsBD.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+/**
+ *
+ * @author 2dami
+ */
+public class ImplementsBD {
+    // Atributos
+	private Connection con;
+	private PreparedStatement stmt;
+
+	// Los siguientes atributos se utilizan para recoger los valores del fich de
+	// configuraci n
+	private ResourceBundle configFile;
+	private String driverBD;
+	private String urlBD;
+	private String userBD;
+	private String passwordBD;
+
+	// dej
+	final String SQLGETMODELS = "INSERT INTO UNIDAD_DIDACTICA VALUES ( ?,?,?,?)";
+        
+        // Para la conexi n utilizamos un fichero de configuaraci n, config que
+	// guardamos en el paquete control:
+	public ImplementsBD() {
+		this.configFile = ResourceBundle.getBundle("model.configClase");
+		this.driverBD = this.configFile.getString("Driver");
+		this.urlBD = this.configFile.getString("Conn");
+		this.userBD = this.configFile.getString("DBUser");
+		this.passwordBD = this.configFile.getString("DBPass");
+	}
+
+	private void openConnection() {
+		try {
+			con = DriverManager.getConnection(urlBD, this.userBD, this.passwordBD);
+		} catch (SQLException e) {
+			System.out.println("Error al intentar abrir la BD");
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+        
+        
+        
+        
+}
