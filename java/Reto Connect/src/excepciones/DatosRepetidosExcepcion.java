a01e35d7bb34a14e2c4e90b66fd896b092376bb8
kev12s <kev12sanz@gmail.com>
2025-09-30 21:57:28 +0200

    agregado paquete de excepciones

---
+++ java/Reto Connect/src/excepciones/DatosRepetidosExcepcion.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package excepciones;
+
+/**
+ *
+ * @author kevin
+ */
+public class DatosRepetidosExcepcion {
+     public DatosRepetidosExcepcion(){
+		      System.out.println("Los datos introducidos ya estan en la base de datos. Prueba con otros datos.");
+	}
+}
